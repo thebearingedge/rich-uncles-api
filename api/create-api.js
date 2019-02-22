3b92ead859c994b8223d4d5c825ad7db140272dc
thebearingedge <contact@timdav.is>
2019-02-21 16:05:23 -0800

    Add cors and helmet to API middleware. #1

--- api/create-api.js
+++ api/create-api.js
@@ -1,9 +1,13 @@
 import express from 'express'
+import cors from 'cors'
+import helmet from 'helmet'
 import { errorHandler } from './util'
 import authRouter from './auth-router'
 
-export default function createApi({ knex, redis }) {
+export default function createApi({ knex }) {
   return express()
+    .use(helmet())
+    .use(cors())
     .use(express.json())
     .use('/auth', authRouter({ knex }))
     .use(errorHandler())
